0249acb775775e8f89a41f1f13d051c9d87ad312
BigBen3918 <benjamin199551@gmail.com>
2022-09-03 07:04:56 -0700

    add: search bar

---
+++ src/components/components/searchModal.js
@@ -0,0 +1,15 @@
+export default function SearchModal(props) {
+    const { className } = props;
+
+    return (
+        <div className={'searchModal ' + className}>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+    );
+}
